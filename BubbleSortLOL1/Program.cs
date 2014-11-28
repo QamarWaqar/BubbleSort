d9d3badfd3c1d46ae07472a60f1b6267d7b67da0
QamarWaqar <qamogle@gmail.com>
2014-11-28 16:29:54 +0500

    Just a bubble sort.
    Nothing Special.

---
+++ BubbleSortLOL1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortLOL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int i, j, temp, min;
+            const int n = 12;
+            int[] x = new int[] { 10, 9, 19, 3, 1, 15, 20, 100, 200, 0, 8, -1 };
+
+            for (j = 0; j < n; j++)
+            {
+                min = x[j];
+                for (i = j; i < n; i++)
+                {
+                    if (x[i] < min)
+                    {
+                        min = x[i];
+                        index = i;
+                    }
+                }
+                temp = x[j];
+                x[j] = min;
+                x[index] = temp;
+            }
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
